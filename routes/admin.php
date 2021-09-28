e9ac4c08f5e9bd1bfb705dc305d8a93925692c46
AR Shahin <mdshahinmije96@gmail.com>
2021-09-28 23:08:58 +0600

    project init

---
+++ routes/admin.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::prefix('admin')->as('admin.')->middleware(['auth:admin'])->group(function () {
+
+    # Dashboard
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
