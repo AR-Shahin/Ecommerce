94da341643826996070150cf3290a871b4a2ac98
AR Shahin <mdshahinmije96@gmail.com>
2021-10-18 20:49:13 +0600

    template mastering

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/shop', function () {
+    return view('shop');
+});
+Route::get('/cart', function () {
+    return view('cart');
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
