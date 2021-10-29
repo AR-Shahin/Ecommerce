2b75fa76d3bd228598c4cb129d886a23a7186eed
AR Shahin <mdshahinmije96@gmail.com>
2021-10-29 18:53:33 +0600

    display category and subcategory

--- resources/views/layouts/frontend_app.blade.php
+++ resources/views/layouts/frontend_app.blade.php
@@ -3,6 +3,7 @@
 <html lang="en">
 @php
     $website = App\Models\Website::first();
+    $categories = App\Models\Category::with('children')->latest()->get();
 @endphp
 <head>
     <meta charset="utf-8">
@@ -135,23 +136,22 @@
                 </a>
                 <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 bg-light" id="navbar-vertical" style="width: calc(100% - 30px); z-index: 999;">
                     <div class="navbar-nav w-100">
+                        @foreach ($categories as $category)
                         <div class="nav-item dropdown dropright">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Dresses <i class="fa fa-angle-right float-right mt-1"></i></a>
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ $category->name }} @if (count($category->children) != 0) <i class="fa fa-angle-right float-right mt-1"></i>
+                                @endif
+                            </a>
+                            @if (count($category->children) != 0)
                             <div class="dropdown-menu position-absolute rounded-0 border-0 m-0">
-                                <a href="" class="dropdown-item">Men's Dresses</a>
-                                <a href="" class="dropdown-item">Women's Dresses</a>
-                                <a href="" class="dropdown-item">Baby's Dresses</a>
+                                @foreach ($category->children as $child)
+                                <a href="" class="dropdown-item">{{ $child->name }}</a>
+                                @endforeach
                             </div>
+                            @endif
                         </div>
-                        <a href="" class="nav-item nav-link">Shirts</a>
-                        <a href="" class="nav-item nav-link">Jeans</a>
-                        <a href="" class="nav-item nav-link">Swimwear</a>
-                        <a href="" class="nav-item nav-link">Sleepwear</a>
-                        <a href="" class="nav-item nav-link">Sportswear</a>
-                        <a href="" class="nav-item nav-link">Jumpsuits</a>
-                        <a href="" class="nav-item nav-link">Blazers</a>
-                        <a href="" class="nav-item nav-link">Jackets</a>
-                        <a href="" class="nav-item nav-link">Shoes</a>
+                        @endforeach
+
+
                     </div>
                 </nav>
             </div>
