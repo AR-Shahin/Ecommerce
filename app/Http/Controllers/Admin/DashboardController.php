d330f6855c5d7db379686d313c24c707ef545517
AR Shahin <mdshahinmije96@gmail.com>
2021-11-01 17:57:10 +0600

    works on dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Coupon;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\ProductInfo;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('Backend.dashboard');
+        $data = [];
+        $data['products'] = Product::withoutGlobalScope('isActive')->count();
+        $data['product_price'] = ProductInfo::sum('price');
+        $data['coupons'] = Coupon::count();
+        $data['orders'] = Order::pending()->count();
+        $data['order_received'] = Order::received()->count();
+        return view('Backend.dashboard', $data);
     }
 }
--- resources/views/Backend/dashboard.blade.php
+++ resources/views/Backend/dashboard.blade.php
@@ -6,65 +6,65 @@
         <!-- small box -->
         <div class="small-box bg-info">
           <div class="inner">
-            <h3>150</h3>
+            <h3>{{ $products }}</h3>
 
-            <p>New Orders</p>
+            <p>Total Product</p>
           </div>
           <div class="icon">
             <i class="ion ion-bag"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="{{ route('admin.product.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <div class="col-lg-3 col-6 ">
         <!-- small box -->
         <div class="small-box bg-success">
           <div class="inner">
-            <h3>150</h3>
+            <h3>{{ $coupons }}</h3>
 
-            <p>New Orders</p>
+            <p>Total Coupon</p>
           </div>
           <div class="icon">
             <i class="ion ion-bag"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="{{ route('admin.coupon.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <div class="col-lg-3 col-6 ">
         <!-- small box -->
         <div class="small-box bg-warning">
           <div class="inner">
-            <h3>150</h3>
+            <h3>{{ $orders }}</h3>
 
             <p>New Orders</p>
           </div>
           <div class="icon">
             <i class="ion ion-bag"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="{{ route('admin.orders.pending') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <div class="col-lg-3 col-6 ">
         <!-- small box -->
         <div class="small-box bg-info">
           <div class="inner">
-            <h3>150</h3>
+            <h3>{{ $order_received }}</h3>
 
-            <p>New Orders</p>
+            <p>Received Orders</p>
           </div>
           <div class="icon">
             <i class="ion ion-bag"></i>
           </div>
-          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          <a href="{{ route('admin.orders.received') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <div class="col-lg-3 col-6 mb-2">
         <!-- small box -->
         <div class="small-box bg-primary">
           <div class="inner">
-            <h3>150</h3>
+            <h3>{{ $product_price }}</h3>
 
-            <p>New Orders</p>
+            <p>Product Price</p>
           </div>
           <div class="icon">
             <i class="ion ion-bag"></i>
